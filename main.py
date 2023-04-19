cdc0a3006c4fcc8dece8d1c68cfe175ae6b5bef6
Llorenç Buil <llorenc.buil@pospotential.com>
2023-04-19 11:22:22 +0200

    backup

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from copy import deepcopy
 
 
-
-
-
 def generate_tree(queue):
-    while len(queue) > 1:
+    queue_aux = deepcopy(queue)
+
+    while len(queue_aux) > 1:
         curr = Node(val=None)
-        curr.l = Node(val=queue.pop()) if type(queue[-1][0]) == str else queue.pop()[0]
-        curr.r = Node(val=queue.pop()) if type(queue[-1][0]) == str else queue.pop()[0]
+        curr.l = (
+            Node(val=queue_aux.pop()) if type(queue_aux[-1][0]) == str
+            else queue_aux.pop()[0]
+        )
+        curr.r = (
+            Node(val=queue_aux.pop()) if type(queue_aux[-1][0]) == str
+            else queue_aux.pop()[0]
+        )
 
         l_char = curr.l.val[0]
         r_char = curr.r.val[0]
@@ -17,8 +22,8 @@ def generate_tree(queue):
 
         curr.val = (l_char + r_char, l_val + r_val)
 
-        queue.append((curr, curr.val[1]))
-        queue = sorted(queue, key=lambda x: x[1], reverse=True)
+        queue_aux.append((curr, curr.val[1]))
+        queue_aux = sorted(queue_aux, key=lambda x: x[1], reverse=True)
 
     return curr
 
@@ -37,11 +42,15 @@ def get_character_frequency(s):
     return queue
 
 
-
-def find_encoding_in_tree(tree):
+def find_encoding_in_tree(tree, queue):
     dict_encoding = {}
 
-    unique_chars = ''.join(set(s))
+    # important: the dict_encoding must contain the string characters ordered
+    # by frequency in descending order (most frequent on the left and least
+    # frequent on the right). This will be important when decoding. The queue
+    # object is already ordered this way.
+    unique_chars = [x[0] for x in queue]
+
     for char in unique_chars:
         char_encoding = ''
         curr = deepcopy(tree)
@@ -58,6 +67,50 @@ def find_encoding_in_tree(tree):
     return dict_encoding
 
 
+def encode_string(s, dict_encoding):
+    encoded_s = ''
+
+    for char in s:
+        encoded_s += dict_encoding[char]
+
+    return encoded_s
+
+
+def decode_string(encoded_s):
+    keys = list(dict_encoding)
+    decoded_s = ''
+    last_i = 0
+    i = 0
+    # iterate over the encoded_s bits
+    #for i in range(len(encoded_s)):
+    while i < len(encoded_s):
+        # iterate over keys
+        for key in keys:
+            i = last_i
+            # exit condition
+            if i >= len(encoded_s):
+                break
+            # iterate over current key bits
+            for k in range(len(dict_encoding[key])):
+                if encoded_s[i] == dict_encoding[key][k]:
+                    if k == len(dict_encoding[key]) - 1:
+                        decoded_s += key
+                        i += 1
+                        last_i = i
+                    i += 1
+                else:
+                    break
+
+    return decoded_s
+
+
+
+
+
+
+
+
+
 class Node:
     def __init__(self, val):
         self.val = val
@@ -66,10 +119,15 @@ class Node:
 
 
 
-s = 'FAAABCBA'
+s = 'AABCBAD'
+
 
 queue = get_character_frequency(s)
 
 tree = generate_tree(queue=queue)
 
-dict_encoding = find_encoding_in_tree(tree=tree)
+dict_encoding = find_encoding_in_tree(tree=tree, queue=queue)
+
+encoded_s = encode_string(s=s, dict_encoding=dict_encoding)
+
+decoded_s = decode_string(encoded_s)
